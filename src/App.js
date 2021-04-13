a70455ff5427522d3221ad1268cda92e2c97ca8b
Osvaldo Borges <netoo70@gmail.com>
2021-04-13 17:02:18 -0300

    Exercicio 01

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
 import React from 'react';
 import './App.css';
 import Cars from './Cars';
+import CarsContext from './myContext';
 
-function App() {
-  return (
-    <Cars />
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      cars: {
+        red: false,
+        blue: false,
+        yellow: false,
+      },
+    }
+  }
+
+  handleChange(name, value) {
+    this.setState((prevState) => ({
+      cars: { ...prevState.cars, [name]: value }
+    }))
+  }
+
+  render() {
+    const contextValue = {
+      cars: this.state.cars,
+      handleChange: this.handleChange      
+    }
+
+    return (
+      <CarsContext.Provider value={contextValue}>
+        <Cars />
+      </CarsContext.Provider>
+    );
+  }
 }
 
 export default App;
--- src/Cars.jsx
+++ src/Cars.jsx
@@ -7,50 +7,55 @@ import carBlue from './images/carBlue.jpeg';
 import carRed from './images/carRed.jpeg';
 import carYellow from './images/carYellow.jpeg';
 import { moveCar } from './redux/actionCreators';
+import CarsContext from './myContext';
 
 function Cars({ redCar, blueCar, yellowCar, moveCar }) {
   return (
-    <div>
-      <div>
-        <img
-          className={redCar ? 'car-right' : 'car-left'}
-          src={carRed}
-          alt="red car"
-        />
-        <button
-          onClick={() => moveCar('red', !redCar)}
-          type="button"
-        >
-          Move
-        </button>
-      </div>
-      <div>
-        <img
-          className={blueCar ? 'car-right' : 'car-left'}
-          src={carBlue}
-          alt="blue car"
-        />
-        <button
-          onClick={() => moveCar('blue', !blueCar)}
-          type="button"
-        >
-          Move
-        </button>
-      </div>
-      <div>
-        <img
-          className={yellowCar ? 'car-right' : 'car-left'}
-          src={carYellow}
-          alt="yellow car"
-        />
-        <button
-          onClick={() => moveCar('yellow', !yellowCar)}
-          type="button"
-        >
-          Move
-        </button>
-      </div>
-    </div>
+    <CarsContext.Consumer>
+      {({cars, handleChange }) => (
+        <div>
+          <div>
+            <img
+              className={cars.red ? 'car-right' : 'car-left'}
+              src={carRed}
+              alt="red car"
+              />
+            <button
+              onClick={() => handleChange('red', !cars.red)}
+              type="button"
+              >
+              Move
+            </button>
+          </div>
+          <div>
+            <img
+              className={cars.blue ? 'car-right' : 'car-left'}
+              src={carBlue}
+              alt="blue car"
+              />
+            <button
+              onClick={() => handleChange('blue', !cars.blue)}
+              type="button"
+              >
+              Move
+            </button>
+          </div>
+          <div>
+            <img
+              className={cars.yellow ? 'car-right' : 'car-left'}
+              src={carYellow}
+              alt="yellow car"
+              />
+            <button
+              onClick={() => handleChange('yellow', !cars.yellow)}
+              type="button"
+              >
+              Move
+            </button>
+          </div>
+        </div>
+      )}
+    </CarsContext.Consumer>
   );
 }
 
